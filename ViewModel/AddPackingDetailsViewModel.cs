d657ac2e4d083a703bfb56f260d5e9d99a77cf31
neha <neha@sswhite.net>
2025-02-27 10:36:39 +0530

    TEst1

---
+++ ViewModel/AddPackingDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PackSlipApp.ViewModel
+{
+    public class AddPackingDetailsViewModel
+    {
+        public List<PackingDetails> PackingList = new List<PackingDetails>();
+    }
+
+    public class PackingDetails
+    {
+        public string Part { get; set; }
+        public string JobNumber { get; set; }
+        public string PartNumber { get; set; }
+        public string Type { get; set; }
+        public bool LM { get; set; }
+        public int Qty { get; set; }
+        
+
+    }
+}
